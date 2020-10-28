4e345e225816522b35c921f2c928b81872f65860
taren_chen@student.uml.edu <taren_chen@student.uml.edu>
2020-10-28 00:41:16 -0400

    added new error case and fix page styling

--- hw4/table.js
+++ hw4/table.js
@@ -1,4 +1,11 @@
 function makeTable() {
+
+    //Clears table and errors
+    var table = ""
+    var errorMsg = ""
+    document.getElementById("errors").innerHTML = errorMsg;
+    document.getElementById("generateTable").innerHTML = table;
+
     var minX = parseInt(document.getElementById("minRows").value);
     var maxX = parseInt(document.getElementById("maxRows").value);
     var minY = parseInt(document.getElementById("minColumns").value);
@@ -8,26 +15,25 @@ function makeTable() {
         "\nThe min Y is: " + minY +
         "\nThe max Y is: " + maxY);
 
-    //Clears table and errors
-    var table = ""
-    var errorMsg = ""
-    document.getElementById("errors").innerHTML = errorMsg;
-    document.getElementById("generateTable").innerHTML = table;
-
     //Error cases
+    if (!Number.isInteger(minX) || !Number.isInteger(maxX) || !Number.isInteger(minY) || !Number.isInteger(maxY)) {
+        var error = "Make sure your inputs are integers."
+        errorMsg += error;
+        errorMsg += "<br>";
+    }
     if (minX > maxX) { // Min is bigger then Max
-        var errorMsg1 = "The minimum row is bigger then the maximum row.";
-        errorMsg += errorMsg1;
+        var error1 = "The minimum row is bigger then the maximum row.";
+        errorMsg += error1;
         errorMsg += "<br>";
     }
     if (minY > maxY) { // Min is bigger then Max
-        var errorMsg2 = "The minimum column is bigger then the maximum column.";
-        errorMsg += errorMsg2;
+        var error2 = "The minimum column is bigger then the maximum column.";
+        errorMsg += error2;
         errorMsg += "<br>";
     }
-    if (minX < -50 || minY < -50 || maxX > 50 || maxY > 50) {
-        var errorMsg3 = "Please keep the values between -50 to 50.";
-        errorMsg += errorMsg3;
+    if (minX < -50 || minY < -50 || maxX > 50 || maxY > 50) { // Bounds
+        var error3 = "Please keep the values between -50 to 50.";
+        errorMsg += error3;
         errorMsg += "<br>";
     }
     if (errorMsg != "") {
